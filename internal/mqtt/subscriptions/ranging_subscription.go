5566cbeebda76ce6394a8d85821dfb492b584907
Martin Peth <martin@peth.pl>
2025-04-29 18:32:28 +0200

    feat(subscription): added mqtt subscriptions with individual handlers

---
+++ internal/mqtt/subscriptions/ranging_subscription.go
@@ -0,0 +1,81 @@
+package subscriptions
+
+import (
+	"context"
+	"encoding/json"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/rs/zerolog"
+	"gps-no-server/internal/logger"
+	"gps-no-server/internal/models"
+	"gps-no-server/internal/repository"
+	"time"
+)
+
+type RangingList []RangingItem
+
+type RangingItem struct {
+	SourceAddress      string       `json:"source_address"`
+	DestinationAddress string       `json:"destination_address"`
+	Distance           DistanceData `json:"distance"`
+}
+
+type DistanceData struct {
+	RawDistance    float64 `json:"raw_distance"`
+	ScaledDistance float64 `json:"scaled_distance"`
+}
+
+type RangingSubscription struct {
+	log               zerolog.Logger
+	rangingRepository *repository.RangingRepository
+	stationRepository *repository.StationRepository
+}
+
+func NewRangingSubscription(rangingRepository *repository.RangingRepository, stationRepository *repository.StationRepository) *RangingSubscription {
+	return &RangingSubscription{
+		log:               logger.GetLogger("ranging-subscription"),
+		rangingRepository: rangingRepository,
+		stationRepository: stationRepository,
+	}
+}
+
+func (c *RangingSubscription) GetTopics() []string {
+	return []string{
+		"gpsno/simulation/devices/+/uwb/ranging",
+	}
+}
+
+func (c *RangingSubscription) HandleMessage(message mqtt.Message) {
+	topic := message.Topic()
+	payload := string(message.Payload())
+
+	var rangingList RangingList
+	if err := json.Unmarshal([]byte(payload), &rangingList); err != nil {
+		c.log.Error().Err(err).Str("topic", topic).Msg("Failed to unmarshal message")
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var rangingModels []*models.Ranging
+	for _, rangingData := range rangingList {
+		source := &models.Station{MacAddress: rangingData.SourceAddress}
+		destination := &models.Station{MacAddress: rangingData.DestinationAddress}
+
+		sourceStation, _ := c.stationRepository.FindByMacAddress(ctx, source.MacAddress)
+		destinationStation, _ := c.stationRepository.FindByMacAddress(ctx, destination.MacAddress)
+
+		rangingModel := &models.Ranging{
+			Source:      sourceStation,
+			Destination: destinationStation,
+			RawDistance: rangingData.Distance.RawDistance,
+		}
+
+		rangingModels = append(rangingModels, rangingModel)
+	}
+
+	if err := c.rangingRepository.SaveAll(ctx, rangingModels); err != nil {
+		c.log.Error().Err(err).Msg("Failed to save ranging data")
+		return
+	}
+}
